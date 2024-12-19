184ac5d31f7d90c315cd1e4240207d357420af65
Surya Narayanaa N T <thangarajsurya001@gmail.com>
2024-12-19 17:03:59 +0530

    Initial commit

---
+++ ca2/arrs.java
@@ -0,0 +1,10 @@
+package ca2;
+
+import java.util.Arrays;
+
+public class arrs {
+    public static void main(String[] args) {
+        int[] arr =   {1,2,3,4,5,6,7,8,9,0};
+        System.out.println(Arrays.toString(arr));
+    }
+}
